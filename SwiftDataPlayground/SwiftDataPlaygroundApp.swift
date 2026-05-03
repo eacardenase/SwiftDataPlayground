4e8e391f7d2f735a1bb7fa724cdf3d6cb21bbc0e
Edwin Cardenas <eacardenase@gmail.com>
2026-05-03 17:53:23 -0500

    Initial Commit

---
+++ SwiftDataPlayground/SwiftDataPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataPlaygroundApp.swift
+//  SwiftDataPlayground
+//
+//  Created by Edwin Cardenas on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
